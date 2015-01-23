ff67d9aaec0e40f5ef2417d07f27b051de2479cc
David Rifkin & Darrin Reeves <pair+dlrifkin.dxr4841@devbootcamp.com>
2015-01-23 11:56:19 -0500

    Create answers table migration.

--- db/migrate/20150123114018_create_answers.rb
+++ db/migrate/20150123114018_create_answers.rb
@@ -1,4 +1,8 @@
 class CreateAnswers < ActiveRecord::Migration
   def change
+    create_table :answers do |t|
+      t.string :answer_content
+      t.references :question
+      t.timestamps
   end
 end
