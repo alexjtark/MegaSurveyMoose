fe759c1f8c780a52d95534ff22ade6b8bb62a4cb
David Rifkin & Darrin Reeves <pair+dlrifkin.dxr4841@devbootcamp.com>
2015-01-23 14:44:51 -0500

    Add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,19 +1,19 @@
-2.times {
+1.times do
   User.create(email: Faker::Internet.email, user_name: Faker::Name.name, password: "a")
-}
+end
 
-4.times {
+2.times do
   Survey.create(title: Faker::Lorem.sentence, creator_id: rand(1..2))
-}
+end
 
-8.times {
+4.times do
   Question.create(content: Faker::Lorem.sentence, survey_id: rand(1..5))
-}
+end
 
-16.times {
-  Answer.create(answer_content: Faker::Lorem.sentence, question_id: rand(1..10))
-}
+8.times do
+  Answer.create(content: Faker::Lorem.sentence, question_id: rand(1..10))
+end
 
-32.times {
+16.times do
   Response.create(answer_id: rand(1..16), taker_id: rand(1..2))
-}
+end
