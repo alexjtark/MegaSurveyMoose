cfb3eb60c0891c894646fa343d194245b1ee7260
David Rifkin & Darrin Reeves <pair+dlrifkin.dxr4841@devbootcamp.com>
2015-01-23 15:37:20 -0500

    Create response and created_survey association in User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,15 @@
 class User < ActiveRecord::Base
   has_secure_password
-
   has_many :surveys
-  has_many :responses
+  has_many :responses, foreign_key: "taker_id"
+
+  has_many :created_surveys, foreign_key: "creator_id", class_name: "Survey"
 
   validates :email, presence: true
   validates :user_name, presence: true
+
+  def taken_surveys
+
+  end
 
 end
