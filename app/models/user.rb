55d57eb41489d3de3dbfc93fe7098fb19517a85f
David Rifkin & Darrin Reeves <pair+dlrifkin.dxr4841@devbootcamp.com>
2015-01-23 12:14:53 -0500

    create model for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ActiveRecord::Base
   has_secure_password
 
+  has_many :surveys
+  has_many :responses
 
+  validates :email, presence: true
+  validates :user_name, presence: true
 
 end
