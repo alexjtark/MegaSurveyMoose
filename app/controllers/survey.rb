9ec4aaa789c5ac7cd3aa788fe4bc9c99e5d529a1
Jon Campbell & sagararathi <pair+heycampbell.sagararathi@devbootcamp.com>
2015-01-23 16:48:05 -0500

    created post method to create new survey

--- app/controllers/survey.rb
+++ app/controllers/survey.rb
@@ -1,5 +1,10 @@
 get '/survey' do
   # takes user to all surveys
+
+end
+
+get '/survey/new', auth: :user do
+  erb :'survey/newSurvey'
 end
 
 get '/survey/:id', auth: :user do |id|
@@ -14,5 +19,19 @@ put '/survey' do
   answers =
   survey
 
+end
+
+
+post '/survey/new' do
+  p params
+  params[:survey][:creator_id => current_user]
+  survey = Survey.create(params[:survey])
+
+  question = Question.create(params[:question])
+  params[:answer].each do |v|
+    question.answers << Answer.create(content: v)
+  end
+  survey.questions << question
+  redirect "/survey/#{survey.id}"
 end
 
