124cf7e9d58d70803532af38fcd1c891d25fc7a3
Jon Campbell & sagararathi <pair+heycampbell.sagararathi@devbootcamp.com>
2015-01-23 11:44:20 -0500

    added controllers for auth, landing, and survey routes

--- app/controllers/auth.rb
+++ app/controllers/auth.rb
@@ -1,11 +1,20 @@
-get '/' do
-  erb :welcome
-end
+
 
 get '/login' do
-  
+
+
 end
 
 get '/signup' do
-  
+
+end
+
+get '/user/:id' do
+  # shows all surveys created by user
+
+end
+
+get '/signout' do
+  session.clear
+  redirect '/'
 end
