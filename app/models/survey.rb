2cce90c9558cc18a75fa3af0b0cd01e90bdf2af9
David Rifkin & Darrin Reeves <pair+dlrifkin.dxr4841@devbootcamp.com>
2015-01-23 15:36:47 -0500

    Create answers and responses associations in survey model

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -2,6 +2,9 @@ class Survey < ActiveRecord::Base
   # Remember to create a migration!
   belongs_to :creator, class_name: "User"
   has_many :questions
+  has_many :answers, through: :questions
+  has_many :responses, through: :answers
 
   validates :title, presence: true
+
 end
