50ccfb5423aae396d2b31150679955ab89c6db84
David Rifkin & Darrin Reeves <pair+dlrifkin.dxr4841@devbootcamp.com>
2015-01-23 16:24:27 -0500

    Create new seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,19 +1,19 @@
-1.times do
+3.times do
   User.create(email: Faker::Internet.email, user_name: Faker::Name.name, password: "a")
 end
 
-2.times do
-  Survey.create(title: Faker::Lorem.sentence, creator_id: rand(1..2))
+6.times do
+  Survey.create(title: Faker::Lorem.sentence, creator_id: rand(1..3))
 end
 
-4.times do
-  Question.create(content: Faker::Lorem.sentence, survey_id: rand(1..2))
+12.times do
+  Question.create(content: Faker::Lorem.sentence, survey_id: rand(1..6))
 end
 
-8.times do
-  Answer.create(content: Faker::Lorem.sentence, question_id: rand(1..4))
+24.times do
+  Answer.create(content: Faker::Lorem.sentence, question_id: rand(1..12))
 end
 
-16.times do
-  Response.create(answer_id: rand(1..16), taker_id: rand(1..2))
+48.times do
+  Response.create(answer_id: rand(1..24), taker_id: rand(1..3))
 end
