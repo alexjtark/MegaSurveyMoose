ad615bf2b81c6942d1ea5391d568a9a6bf27009a
David Rifkin <davidlrifkin@gmail.com>
2015-01-25 23:17:37 -0500

    Comment explanation of user model method to return all quizzes taken by user, accounting for multiples

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,6 +17,7 @@ class User < ActiveRecord::Base
 
   def every_individual_survey_taken_by_user
     return self.sorted_responses_to_all_surveys.map {|s| s.group_by{|v| v.created_at.to_i}.values}
+      # Returns an array of arrays of arrays. The "outer" array holds all of a user's responses to every survey, regardless of how many times they responded to that survey. The "inner" arrays hold one instance of a user's responses to a particular survey. The "intermediate" arrays group together the "inner" arrays by survey, for access from the "outer" array. Will demonstrate usage as explicitly as possible in view file, but obviously this method/its return can easily be refactored at some later point.
   end
 
 end
